54531fa8069380ce541fc1552ebf1875b959adcc
daveS <david.surina22@gmail.com>
2024-09-09 13:39:05 +0200

    basic news page

---
+++ app/news/page.js
@@ -0,0 +1,17 @@
+export const metadata = {
+  title: "All meals",
+  description: "Browse delicious meals shared by users.",
+};
+
+export default function NewsPage() {
+  return (
+    <>
+      <header className="page-header">
+        <h1>All the news</h1>
+      </header>
+      <main>
+        <p>dummy news</p>
+      </main>
+    </>
+  );
+}
