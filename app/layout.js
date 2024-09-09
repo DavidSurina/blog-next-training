18be5ddfe6b70d622c92d20d58f831f1d1f4f1bd
daveS <david.surina22@gmail.com>
2024-09-09 17:11:13 +0200

    updated main layout

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,20 @@
 import MainHeader from "@/components/main-header";
-import './globals.css';
+import "./globals.css";
 
 export const metadata = {
-  title: 'Next.js Page Routing & Rendering',
-  description: 'Learn how to route to different pages.',
-}
+  title: "Next.js Page Routing & Rendering",
+  description: "Learn how to route to different pages.",
+};
 
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="en">
-        <MainHeader />
-        <body>{children}</body>
+      <body>
+        <div id="page">
+          <MainHeader />
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
