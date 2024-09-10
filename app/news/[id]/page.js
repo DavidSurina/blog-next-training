e54dc79427340216b5d5e8cda381f019ec97dd99
daveS <david.surina22@gmail.com>
2024-09-10 16:34:12 +0200

    slug news page population

--- app/news/[id]/page.js
+++ app/news/[id]/page.js
@@ -1,3 +1,16 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+
 export default function NewsDetailPage({ params }) {
-  return <p>{params.page}</p>;
+  const newsItem = DUMMY_NEWS.find((item) => item.slug === params.slug);
+
+  return (
+    <article>
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
+  );
 }
